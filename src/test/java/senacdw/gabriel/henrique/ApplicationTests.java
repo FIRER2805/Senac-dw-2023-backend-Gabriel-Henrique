1570544c140f277b80147ea048aa5bab4991cdd7
gabriel.medeiros3 <gabriel.medeiros3@FLE104D11.alunos.sc.senac.br>
2023-08-16 09:03:13 -0300

    setup do projeto

---
+++ src/test/java/senacdw/gabriel/henrique/ApplicationTests.java
@@ -0,0 +1,13 @@
+package senacdw.gabriel.henrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
