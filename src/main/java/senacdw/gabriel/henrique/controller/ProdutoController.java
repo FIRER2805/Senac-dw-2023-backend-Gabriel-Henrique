f86f07c1e865b1a02cc70cc896ff052d83a25131
Gabriel Henrique Muller <gabriel_henrique2805@hotmail.com>
2023-09-05 11:10:00 -0300

    arrumando bugs

--- src/main/java/senacdw/gabriel/henrique/controller/ProdutoController.java
+++ src/main/java/senacdw/gabriel/henrique/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package senacdw.gabriel.henrique.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import senacdw.gabriel.henrique.service.ProdutoService;
 
 @RestController
 @RequestMapping(path = "/api/produtos")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:5500"}, maxAge = 3600)
 public class ProdutoController {
 
 	@Autowired
